266aeb7797def9a6dafcc33ff4eebb41eb5314b3
maria <maria.olsson92@hotmail.se>
2021-10-16 12:18:28 +0200

    Polished FileReader

--- first/FileReader.cpp
+++ first/FileReader.cpp
@@ -5,7 +5,7 @@ void FileReader::readFile(std::string fileName, std::vector<int>& vec) {
     std::ifstream read_file(fileName);
     assert(read_file.is_open());
 
-    std::copy(std::istream_iterator<double>(read_file), std::istream_iterator<double>(),
+    std::copy(std::istream_iterator<int>(read_file), std::istream_iterator<int>(),
         std::back_inserter(vec));
 
     read_file.close();
